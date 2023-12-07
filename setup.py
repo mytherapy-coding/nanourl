000e59516d55fa5fa740d9c4b3aa2a99a8ac9ba7
Alena <102840105+mytherapy-coding@users.noreply.github.com>
2023-12-07 14:27:31 -0500

    fixed black setup warning

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='nanourl',
-    version='0.1.0',
-    packages=find_packages(include=['url_shortener.py', 'main.py']),
+    name="nanourl",
+    version="0.1.0",
+    packages=find_packages(include=["url_shortener.py", "main.py"]),
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
 )
